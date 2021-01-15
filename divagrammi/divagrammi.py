3945ad1fac74eeef9ecc6f2e0d6d0d244c86fa80
Erik Petrov <koteman123@hotmail.com>
2021-01-15 14:05:47 +0200

    Add project files.

---
+++ divagrammi/divagrammi.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+x = np.arange(-12,12,1)
+y = -1/18*x**2+12
+
+x1 = np.arange(-4,4,1)
+y1 = -1/8*x1**2+6
+
+x2 = np.arange(-12,-4,1)
+y2 = -1/8*(x2+8)**2+6
+
+x3 = np.arange(4,12,1)
+y3 = -1/8*(x3-8)**2+6
+
+x4 = np.arange(-4,-0.3,1)
+y4 = 2*(x4+3)**2-9
+
+x5 = np.arange(-4,0.2,1)
+y5 = 1.5*(x5+3)**2-10
+
+plt.subplots()
+plt.title("Зонтик")
+plt.tick_params(axis="x", direction="in",length=5, width=5,color="red",labelsize=7)
+plt.grid(True)
+plt.plot(x,y,x1,y1,x2,y2,x3,y3,x4,y4,x5,y5 ,'-r',linewidth=5)
+plt.show()
