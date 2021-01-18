c11d54cc7c6f6f51e111673051c6e4b9e2259778
Erik Petrov <koteman123@hotmail.com>
2021-01-18 14:29:00 +0200

    добавлена диаграмма

--- divagrammi/divagrammi.py
+++ divagrammi/divagrammi.py
@@ -25,4 +25,20 @@ plt.title("Зонтик")
 plt.tick_params(axis="x", direction="in",length=5, width=5,color="red",labelsize=7)
 plt.grid(True)
 plt.plot(x,y,x1,y1,x2,y2,x3,y3,x4,y4,x5,y5 ,'-r',linewidth=5)
+plt.show
+#-----------------------------------------------------------------------------------------
+with open("dannie.txt","r") as f:
+    strs=[]
+    ints=[]
+    for line in f:
+        n=line.find(",")
+        strs.append(line[0:n].strip())
+        ints.append(int(line[n+1:len(line)].strip()))
+
+title = "Данные о ИТ безопасности"
+plt.grid(True)
+
+color_rectangle = np.random.rand(7, 3)
+plt.barh(strs, ints, color=color_rectangle)
+
 plt.show()
